9d6cbf5993e34bac9fd114d72d94a80843c3a481
wcpierce <wcpierce@d7b3437e-3345-0410-94a8-cbd290e69f67>
2008-10-19 13:35:49 +0000

    This release uses NH 2.0.1.  Trunk will move forward with 2.1.0

---
+++ tags/NHibernate.ProxyGenerators/1.0.0Beta622/src/NHibernate.ProxyGenerators/ProxyGeneratorException.cs
@@ -0,0 +1,33 @@
+﻿namespace NHibernate.ProxyGenerators
+{
+	using System;
+	using System.Runtime.Serialization;
+
+	[Serializable]
+	public class ProxyGeneratorException : Exception
+	{
+		public ProxyGeneratorException()
+		{
+		}
+
+		public ProxyGeneratorException(string message)
+			: base(message)
+		{
+		}
+
+		public ProxyGeneratorException(string messageFormat, params object[] args)
+			: base(string.Format(messageFormat, args))
+		{
+		}
+
+		protected ProxyGeneratorException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+
+		public ProxyGeneratorException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+	}
+}
