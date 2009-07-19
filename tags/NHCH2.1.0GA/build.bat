6d2f2c4ebce5a516a7be10fb7df61f7d8372c149
fabiomaulo <fabiomaulo@d7b3437e-3345-0410-94a8-cbd290e69f67>
2009-07-19 14:00:14 +0000

    Release 2.1.0GA

---
+++ tags/NHCH2.1.0GA/build.bat
@@ -0,0 +1,25 @@
+cd SysCache
+nant -t:net-3.5
+cd ..
+
+cd SysCache2
+nant -t:net-3.5
+cd ..
+
+cd SharedCache
+nant -t:net-3.5
+cd ..
+
+cd MemCache
+nant -t:net-3.5
+cd ..
+
+cd Velocity
+nant -t:net-3.5
+cd ..
+
+cd Prevalence
+nant -t:net-3.5
+cd ..
+
+pause
